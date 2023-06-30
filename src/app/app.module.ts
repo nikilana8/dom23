e4b09d33513d534272b29a140edc253af6fc2141
svetlana <lana8lana@gmail.com>
2023-06-30 21:19:55 +0300

    docs folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {HttpClientModule} from "@angular/common/http";
+import {ReactiveFormsModule} from "@angular/forms";
+import {NgbModule} from "@ng-bootstrap/ng-bootstrap";
+import {SharedModule} from "./shared/shared.module";
+import {MainModule} from "./features/components/main/main.module";
+import {OrderModule} from "./features/components/order/order.module";
+import {ProductsModule} from "./features/components/products/products.module";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    NgbModule,
+    SharedModule,
+    MainModule,
+    OrderModule,
+    ProductsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
